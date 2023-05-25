43f0dc635811393a454e29caab2d4eb72b5abdf1
prasannasrinivasan-sudo <115617814+prasannasrinivasan-sudo@users.noreply.github.com>
2023-05-25 21:48:05 +0530

    Add files via upload

---
+++ src/gps_coordinates_retrieval.py
@@ -0,0 +1,42 @@
+import math
+import pandas as pd
+
+initial_lat = 12.832712
+initial_lon = 80.050148
+
+earth_radius = 6371 
+time_step = 0.18 
+
+initial_velocity = 0
+theta = math.pi/4
+
+mpudata = pd.read_csv(r'C:\Users\iampr\Downloads\major_project\dataset\mpusensor.TXT')
+mpudata_len = len(mpudata)+1
+
+for i in range (0,mpudata_len):
+    x_axis_acc, y_axis_acc, z_axis_acc = pd.read_csv(r'C:\Users\iampr\Downloads\major_project\dataset\mpusensor.TXT', usecols=['-0.60', ' -0.24', ' 0.05'])
+    x_axis_gyr, y_axis_gyr, z_axis_gyr = pd.read_csv(r'C:\Users\iampr\Downloads\major_project\dataset\mpusensor.TXT', usecols=['-20.78', ' -10.14', ' -26.12'])
+    x_axis_acc=float(x_axis_acc)
+    y_axis_acc=float(y_axis_acc)
+    z_axis_acc=float(z_axis_acc)
+    x_axis_gyr=float(x_axis_gyr)
+    y_axis_gyr=float(y_axis_gyr)
+    z_axis_gyr=float(z_axis_gyr)
+   
+    mag_acc_vector = math.sqrt(x_axis_acc**2 + y_axis_acc**2 + z_axis_acc**2)
+    avg_angular_velocity = (x_axis_gyr*time_step + y_axis_gyr*time_step + z_axis_gyr*time_step)/3
+
+    initial_velocity += mag_acc_vector*time_step
+    theta += avg_angular_velocity / math.cos(initial_lat)
+    
+    initial_lat += (initial_velocity*time_step/earth_radius) * math.cos(theta)
+    initial_lon += (initial_velocity*time_step/earth_radius) * math.sin(theta) / math.cos(initial_lat)
+    
+    with open(r'C:\Users\iampr\Downloads\major_project\dataset\new_gps_coordinates.txt', 'a') as f:
+        f.write(str(initial_lat))
+        f.write(',')
+        f.write(str(initial_lon))
+        f.write("\n")
+        f.close()
+
+    print("Current position: ", initial_lat, initial_lon)
---
+++ src/mpu9250_arduino.ino
@@ -0,0 +1,133 @@
+#include "MPU9250.h"
+#include <SPI.h>
+#include <SD.h>
+#include <NMEAGPS.h>
+#include <GPSport.h>
+
+File myFile;
+MPU9250 mpu;
+NMEAGPS  gps; 
+gps_fix  fix; 
+
+void setup() {
+    Serial.begin(9600);
+    Wire.begin();
+    delay(2000);
+
+    while (!Serial) {
+    ;
+    }
+    Serial.println("Neo-6m gps module initializing");
+    gpsPort.begin(9600);
+    delay(5000);
+    
+    Serial.println("Initializing SD card");
+    if (!SD.begin(10)) {
+    Serial.println("Initialization failed!");
+    while (1);
+    }
+    Serial.println("Initialization done.");
+
+
+
+    if (!mpu.setup(0x68)) {  
+        while (1) {
+            Serial.println("MPU connection failed. Please check your connection");
+            delay(5000);
+        }
+    }
+
+    
+    Serial.println("Accel Gyro calibration will start in 5sec.");
+    Serial.println("Please leave the device still on the flat plane.");
+    mpu.verbose(true);
+    delay(5000);
+    mpu.calibrateAccelGyro();
+
+    Serial.println("Mag calibration will start in 5sec.");
+    Serial.println("Please Wave device until calibration done.");
+    delay(5000);
+    mpu.calibrateMag();
+    mpu.verbose(false);
+}
+
+void loop() {
+    if (mpu.update()) {
+        static uint32_t prev_ms = millis();
+        if (millis() > prev_ms + 25) {
+            print_roll_pitch_yaw();
+            prev_ms = millis();
+        }
+    }
+    while (gps.available( gpsPort )) {
+        fix = gps.read();
+    }
+}
+
+void print_roll_pitch_yaw() {
+    Serial.print("Lat, Lon, Alt: ,");
+    Serial.print( fix.latitude(), 6 );
+    Serial.print( ',' );
+    Serial.print( fix.longitude(), 6 );
+    Serial.print( ',' );
+    Serial.print( fix.altitude() );
+    Serial.print( ',' );
+    Serial.print("Yaw, Pitch, Roll: ,");
+    Serial.print(mpu.getYaw(), 2);
+    Serial.print(", ");
+    Serial.print(mpu.getPitch(), 2);
+    Serial.print(", ");
+    Serial.print(mpu.getRoll(), 2);
+    Serial.print(", ");
+    Serial.print("Mag : ,");
+    Serial.print(mpu.getMagX(), 2);
+    Serial.print(", ");
+    Serial.print(mpu.getMagY(), 2);
+    Serial.print(", ");
+    Serial.print(mpu.getMagZ(), 2);
+    Serial.print(", ");
+    Serial.print("lin_acc = ,");
+    Serial.print(mpu.getLinearAccX(), 2);
+    Serial.print(", ");
+    Serial.print(mpu.getLinearAccY(), 2);
+    Serial.print(", ");
+    Serial.println(mpu.getLinearAccZ(), 2);
+
+
+    myFile = SD.open("mpu9250.txt", FILE_WRITE);
+    if (myFile) {
+
+        myFile.print("Lat, Lon, Alt: ,");
+        myFile.print( fix.latitude(), 6 );
+        myFile.print( ',' );
+        myFile.print( fix.longitude(), 6 );
+        myFile.print( ',' );
+        myFile.print( fix.altitude() );
+        myFile.print( ',' );
+        myFile.print("Yaw, Pitch, Roll: ,");
+        myFile.print(mpu.getYaw(), 2);
+        myFile.print(", ");
+        myFile.print(mpu.getPitch(), 2);
+        myFile.print(", ");
+        myFile.print(mpu.getRoll(), 2);
+        myFile.print(", ");
+        myFile.print("Mag : ,");
+        myFile.print(mpu.getMagX(), 2);
+        myFile.print(", ");
+        myFile.print(mpu.getMagY(), 2);
+        myFile.print(", ");
+        myFile.print(mpu.getMagZ(), 2);
+        myFile.print(", ");
+        myFile.print("lin_acc = ,");
+        myFile.print(mpu.getLinearAccX(), 2);
+        myFile.print(", ");
+        myFile.print(mpu.getLinearAccY(), 2);
+        myFile.print(", ");
+        myFile.println(mpu.getLinearAccZ(), 2);
+        myFile.close();
+        Serial.println("done.");
+        } else {
+        Serial.println("error opening mpu9250.txt");
+        }
+
+}
